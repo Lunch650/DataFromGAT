e8537433e5ce474fd2ceb9617b8f73ba35289347
Lunch650 <lunch650@qq.com>
2017-07-26 00:00:28 +0800

    Update DataFromGAT.java

--- src/DataFromGAT.java
+++ src/DataFromGAT.java
@@ -1,9 +1,19 @@
+import queryservice.XSJJQueryService;
 
 /**
  * Author : Lunch
  * Data : 2017/7/23 上午12:57
  */
 public class DataFromGAT {
-  public static void main(String args[]){
+  public static void main(String[] args){
+
+    String XSJJQuery = "SFZHM LIKE '520423%'";
+    XSJJQueryService xsjj = new XSJJQueryService(XSJJQuery);
+    DataSource XSJJ = new DataSource(xsjj.XSJJArgs);
+    try{
+      XSJJ.parsejgbw();
+    }catch (Exception e){
+      e.printStackTrace();
+    }
   }
 }
