cbbe6def77746364ef397fcaeab4a6c711d8ae14
Lunch <Lunch650@qq.com>
2017-07-29 00:23:41 +0800

    创建BaseQueryService父类，各查询服务继承其中的报文拼接、RPCClient等方法

--- src/GetSource.java
+++ src/GetSource.java
@@ -21,84 +21,54 @@ class DataSource{
   public DataSource(HashMap queryMap) {
     //将报文参数加入到请求报文
     queryArgs = queryMap;
-    this.qqbw =""//用*过滤掉敏感字符
+    qqbw =
             "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-            "<SRmessage>\n" +
-            "  <FWQQ_BWBH></FWQQ_BWBH>\n" +
-            "  <FWQQZ_ZCXX>\n" +
-            "    <FWQQZMC>镇宁****</FWQQZMC>\n" +
-            "    <FWQQZBS></FWQQZBS>\n" +
-            "    <FWQQZRKDZLB>\n" +
-            "      <FWQQZ_IPDZ>***</FWQQZ_IPDZ>\n" +
-            "      <FWQQZ_ZXDKHM>***</FWQQZ_ZXDKHM>\n" +
-            "    </FWQQZRKDZLB>\n" +
-            "    <FWQQZ_LXFS>\n" +
-            "      <LXR_XM>梁*</LXR_XM>\n" +
-            "      <LXR_DH>183***</LXR_DH>\n" +
-            "      <LXR_GMSFHM>52***</LXR_GMSFHM>\n" +
-            "    </FWQQZ_LXFS>\n" +
-            "  </FWQQZ_ZCXX>\n" +
-            "  <FWBS>" + queryArgs.get("FWBS") + "</FWBS>\n" +
-            "  <FW_ZXBS></FW_ZXBS>\n" +
-            "  <FWQQ_RQSJ>20170718161550</FWQQ_RQSJ>\n" +
-            "  <FWQQ_BZ>\n" +
-            "    <FWDYID>" + queryArgs.get("FWDYID") + "</FWDYID>\n" +
-            "    <Items>\n" +
-            "      <Item>\n" +
-            "        <Data>" + queryArgs.get("query") + "</Data>\n" +
-            "        <Data>1</Data>\n" +
-            "        <Data>10</Data>\n" +
-            "        <Data>0</Data>\n" +
-            "      </Item>\n" +
-            "      <Item>\n" +
-            "        <Row>\n" + queryArgs.get("queryColumn")+
-            "        </Row>\n" +
-            "      </Item>\n" +
-            "    </Items>\n" +
-            "  </FWQQ_BZ>\n" +
-            "  <XXCZRY_XM>梁*</XXCZRY_XM>\n" +
-            "  <XXCZRY_GMSFHM>52***</XXCZRY_GMSFHM>\n" +
-            "  <XXCZRY_GAJGJGDM>520423110000</XXCZRY_GAJGJGDM>\n" +
-            "  <XXCZRY_ZWJBDM></XXCZRY_ZWJBDM>\n" +
-            "  <XXCZRY_GWQJDM></XXCZRY_GWQJDM>\n" +
-            "  <XXCZRY_XZQHDM></XXCZRY_XZQHDM>\n" +
-            "  <XXCZRY_MMDJDM></XXCZRY_MMDJDM>\n" +
-            "  <XXCZRY_JZDM></XXCZRY_JZDM>\n" +
-            "  <YBQQ_BZDM></YBQQ_BZDM>\n" +
-            "  <JZQX></JZQX>\n" +
-            "</SRmessage>\n";
+                    "<SRmessage>\n" +
+                    "  <FWQQ_BWBH></FWQQ_BWBH>\n" +
+                    "  <FWQQZ_ZCXX>\n" +
+                    "    <FWQQZMC>镇宁智能实战应用平台</FWQQZMC>\n" +
+                    "    <FWQQZBS>A00052042311000000001</FWQQZBS>\n" +
+                    "    <FWQQZRKDZLB>\n" +
+                    "      <FWQQZ_IPDZ>10.164.0.69</FWQQZ_IPDZ>\n" +
+                    "      <FWQQZ_ZXDKHM>8080</FWQQZ_ZXDKHM>\n" +
+                    "    </FWQQZRKDZLB>\n" +
+                    "    <FWQQZ_LXFS>\n" +
+                    "      <LXR_XM>梁纯</LXR_XM>\n" +
+                    "      <LXR_DH>18334028890</LXR_DH>\n" +
+                    "      <LXR_GMSFHM>522526198706050036</LXR_GMSFHM>\n" +
+                    "    </FWQQZ_LXFS>\n" +
+                    "  </FWQQZ_ZCXX>\n" +
+                    "  <FWBS>" + queryArgs.get("FWBS") + "</FWBS>\n" +
+                    "  <FW_ZXBS></FW_ZXBS>\n" +
+                    "  <FWQQ_RQSJ>20170718161550</FWQQ_RQSJ>\n" +
+                    "  <FWQQ_BZ>\n" +
+                    "    <FWDYID>" + queryArgs.get("FWDYID") + "</FWDYID>\n" +
+                    "    <Items>\n" +
+                    "      <Item>\n" +
+                    "        <Data>" + queryArgs.get("query") + "</Data>\n" +
+                    "        <Data>1</Data>\n" +
+                    "        <Data>10</Data>\n" +
+                    "        <Data>0</Data>\n" +
+                    "      </Item>\n" +
+                    "      <Item>\n" +
+                    "        <Row>\n" + queryArgs.get("queryColumn")+
+                    "        </Row>\n" +
+                    "      </Item>\n" +
+                    "    </Items>\n" +
+                    "  </FWQQ_BZ>\n" +
+                    "  <XXCZRY_XM>梁纯</XXCZRY_XM>\n" +
+                    "  <XXCZRY_GMSFHM>522526198706050036</XXCZRY_GMSFHM>\n" +
+                    "  <XXCZRY_GAJGJGDM>520423110000</XXCZRY_GAJGJGDM>\n" +
+                    "  <XXCZRY_ZWJBDM></XXCZRY_ZWJBDM>\n" +
+                    "  <XXCZRY_GWQJDM></XXCZRY_GWQJDM>\n" +
+                    "  <XXCZRY_XZQHDM></XXCZRY_XZQHDM>\n" +
+                    "  <XXCZRY_MMDJDM></XXCZRY_MMDJDM>\n" +
+                    "  <XXCZRY_JZDM></XXCZRY_JZDM>\n" +
+                    "  <YBQQ_BZDM></YBQQ_BZDM>\n" +
+                    "  <JZQX></JZQX>\n" +
+                    "</SRmessage>\n";
   }
 
-  public DataSource(HashMap queryMap,boolean oldVersion){
-    this(queryMap);
-    this.qqbw = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-              "<SinoMessage>\n" +
-              "  <ResourceID>"+queryArgs.get("FWDYID")+"</ResourceID>\n" +
-              "  <ServiceID>"+queryArgs.get("FWBS")+"</ServiceID>\n" +
-              "  <Method>\n" +
-              "    <Name>QUERY</Name>\n" +
-              "    <Security>\n" +
-              "      <Signature></Signature>\n" +
-              "      <Encrypt>**</Encrypt>\n" +
-              "    </Security>\n" +
-              "    <Items>\n" +
-              "      <Item>\n" +
-              "        <Data>"+queryArgs.get("query")+"</Data>\n" +
-              "        <Data>1</Data>\n" +
-              "        <Data>10</Data>\n" +
-              "        <Data>0</Data>\n" +
-              "        <Data>梁*</Data>\n" +
-              "        <Data>522***</Data>\n" +
-              "        <Data>520423110000</Data>\n" +
-              "      </Item>\n" +
-              "      <Item>\n" +
-              "        <Row>\n" + queryArgs.get("queryColumn")+
-              "        </Row>\n" +
-              "      </Item>\n" +
-              "    </Items>\n" +
-              "  </Method>\n" +
-              "</SinoMessage>\n";
-    }
 
   private String getResource() throws Exception{
     //接口地址，修改成实际地址
@@ -117,7 +87,7 @@ class DataSource{
     Class[] classes = new Class[] { String.class };
     //指定调用的方法及WSDL文件的命名空间
     QName opAddEntry = new QName(
-            "http://sinoService.***",
+            "http://sinoService.sjfwpt.jzpt.sinobest.cn",
             "queryService");
     //调用queryService方法并输出该方法的返回值,
     //返回对象是一个Object的数组,拿数组的第一个值，转换强转即可
@@ -125,20 +95,5 @@ class DataSource{
             opAddEntryArgs, classes)[0].toString();
     return bw;
   }
-
-  public void parsejgbw(){
-    try{
-      String jgbw = getResource();
-      Document document = DocumentHelper.parseText(jgbw);
-      List<Element> receiveRow = document.selectNodes("/SBmessage/ZXBW_BZ/SPmessage/FWTG_BZ/Items/Item/Value/Row/Data");
-      this.totalNum = Integer.parseInt(receiveRow.get(0).getText());
-      for (int i = 2; i < receiveRow.size(); i++) {
-        System.out.println(receiveRow.get(i).elementText("Normal"));
-      }
-    }catch (Exception e){
-      e.printStackTrace();
-    }
-  }
-
 
 }
