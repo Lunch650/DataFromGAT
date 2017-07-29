34948230299d666ae156a9e4f867a765e017cdb1
Lunch <Lunch650@qq.com>
2017-07-29 15:52:46 +0800

    #将GetSource.java中的方法写入BaseQueryService文件，删除GetSource
    #以后由子类直接继承的方式调用数据获取方法。

--- src/DataFromGAT.java
+++ src/DataFromGAT.java
@@ -8,10 +8,10 @@ public class DataFromGAT {
   public static void main(String[] args){
 
     String XSJJQuery = "SFZHM LIKE '520423%'";
-    XSJJQueryService xsjj = new XSJJQueryService(XSJJQuery);
-    DataSource XSJJ = new DataSource(xsjj.XSJJArgs);
+    XSJJQueryService xsjj = new XSJJQueryService(XSJJQuery,false);
+    /*DataSource XSJJ = new DataSource(xsjj.XSJJArgs);*/
     try{
-      XSJJ.parsejgbw();
+      System.out.print(xsjj.XSJJArgs.get("qqbw"));
     }catch (Exception e){
       e.printStackTrace();
     }
