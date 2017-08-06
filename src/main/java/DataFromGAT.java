3e98a003f2f82ff5e6ebc39ba0e0f4362129fe9d
Lunch <Lunch650@qq.com>
2017-08-06 23:23:05 +0800

    Why:从基础开始，不再使用maven框架与mybatis

--- src/main/java/DataFromGAT.java
+++ src/main/java/DataFromGAT.java
@@ -1,11 +1,3 @@
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import pojo.Test;
-
-import java.io.InputStream;
-import java.util.List;
 
 /**
  * Author : Lunch
@@ -13,28 +5,9 @@ import java.util.List;
  */
 public class DataFromGAT {
   public static void main(String[] args){
-    InputStream inputStream = null;
-    try {
-      inputStream = Resources.getResourceAsStream("mybatis/mybatis-config.xml");
-    }catch (Exception e) {
-      e.printStackTrace();
-    }
-      SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
-      SqlSession session = factory.openSession();
-    try{
-      //session.insert("mybatis.mapper.testMapper.insertTest",user);
-      List<String> test = session.selectList("mybatis.mapper.testMapper.showColumns");
-      session.commit();
-      System.out.print(test);
-    }catch (Exception e){
-      e.printStackTrace();
-    }finally {
-      session.close();
-
-    }
 
 
-/*    String XSJJQuery = "SFZHM LIKE '520423%'";
+/*  String XSJJQuery = "SFZHM LIKE '520423%'";
     XSJJQueryService xsjj = new XSJJQueryService(XSJJQuery,false);
     *//*DataSource XSJJ = new DataSource(xsjj.XSJJArgs);*//*
     try{
